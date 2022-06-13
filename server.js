c49acf0ef23f95c8b4cf416ec5ed09dc3e8219b5
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2022-06-13 12:39:49 +1000

    implemnt controllers arcturtur for clearner code

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
 const { json } = require('express');
+const app = express();
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const images = require('./controllers/images')
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -12,7 +19,6 @@ const db = knex({
     }
 });
 
-const app = express();
 
 app.use(express.json())
 app.get('/', (req, res) => {
@@ -20,84 +26,10 @@ app.get('/', (req, res) => {
    
 })
 
-app.post('/signin', (req, res) => {
-db.select('email','hash').from('login')
-.where('email','=',req.body.email)
-.then(data =>{
-   const isValid = bcrypt.compareSync(req.body.password,data[0].hash)
-   if(isValid){
-    db.select('*').from('users')
-    .where('email','=', req.body.email)
-    .then(user=>{
-        res.json(user[0])
-    })
-    .catch(err =>{
-        res.status(400).json('unable to get user')
-    })
-   }else{
-   res.status(400).json('wrong password')
-   }
-})
-.catch(err => res.status(400).json('wrong password'))
-})
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx
-            .insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0].email,
-                        name: name,
-                        joined: new Date()
-                    }).then(user => {
-                        res.json(user[0])
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-    })
-        .catch(err => {
-            res.status(400).json('unable to registor');
-        })
-
-})
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*')
-        .from('users')
-        .where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json('user not found')
-            }
-
-        })
-        .catch(err => {
-            res.status(400).json('error gettimg user')
-        })
-
-})
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0].entries)
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
-})
+app.post('/signin', (req,res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req,res)=> {profile.handleProfileGet(req,res,db)})
+app.put('/image', (req,res)=>{images.handleNewEntry(req,res,db)})
 
 app.listen(3000, () => {
     console.log('app is listening on port 3000.....');
