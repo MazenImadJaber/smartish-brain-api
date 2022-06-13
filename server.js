610c9559531857ae37359ee735d3879e3decdd77
Mazen Imad Jaber <57973260+MazenImadJaber@users.noreply.github.com>
2022-06-13 11:01:04 +1000

    adding the db to the regestor, get by id and image endpoints

--- server.js
+++ server.js
@@ -1,43 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-const knex  = require('knex')
-const postgres = knex({
+const knex = require('knex');
+const { json } = require('express');
+const db = knex({
     client: 'pg',
-    connection:{
+    connection: {
         host: '127.0.0.1',
         user: '',
         password: '',
         database: 'smart-brain'
     }
 });
-postgres.select('*').from('users')
 
 const app = express();
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'john',
-            email: 'john@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '789',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
+
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send(database.users)
@@ -54,47 +30,51 @@ app.post('/signin', (req, res) => {
 })
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password,null , null, function(err,hash){
+    bcrypt.hash(password, null, null, function (err, hash) {
         // STORE HASH IN DB
         console.log(hash)
     });
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length - 1])
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        }).then(user => {
+            res.json(user[0])
+        })
+        .catch(err => {
+            res.status(400).json('unable to registor');
+        })
+
 })
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            res.json(user);
-            found = true;
-        }
-    });
-    if (!found) {
-        res.status(400).json('no such user');
-    }
+    db.select('*')
+        .from('users')
+        .where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else {
+                res.status(400).json('user not found')
+            }
+
+        })
+        .catch(err => {
+            res.status(400).json('error gettimg user')
+        })
 
 })
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(400).json('no such user');
-    }
+    db('users').where('id','=',id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries =>{
+        res.json(entries[0].entries)
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {
